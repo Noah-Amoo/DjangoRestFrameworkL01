48076bffa1fd1f0ee9fde2d5c2192806b92a67f1
Noah Amoo <yawnoah3n@yahoo.com>
2025-06-02 00:48:39 +0100

    Added the post function in views to show created coupons

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 
 from .models import Coupon
 from .serializers import CouponSerializer
@@ -9,3 +10,9 @@ class CouponAPIView(APIView):
         coupons = Coupon.objects.all()
         serializer = CouponSerializer(coupons, many=True)
         return Response({"coupons": serializer.data})
+    
+    def post(self, request):
+        serializer = CouponSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)

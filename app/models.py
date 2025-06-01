d9fb6df8c9048d8c193a2a41c5dbd22ec5917d20
Noah Amoo <yawnoah3n@yahoo.com>
2025-06-01 15:40:33 +0100

    Added the coupon model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Coupon(models.Model):
+    code = models.CharField(max_length=10, unique=True)
+    discount = models.IntegerField()
+    active = models.BooleanField()
+
+    def __str__(self):
+        return self.code
